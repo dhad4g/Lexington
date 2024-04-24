c6c0170912c98b9970035eaf3f461dbcd96a173a
A02324248 <a02324248@el103-07.ece.usu.edu>
2024-04-24 11:07:24 -0600

    Working on testbench pt. 2

--- rtl/peripheral/ps2_controller.sv
+++ rtl/peripheral/ps2_controller.sv
@@ -4,9 +4,9 @@ module ps2_controller (
     input ps2_clk,
     input ps2_data,
 
-    output reg [7:0] rx_data,
-    output reg rx_valid,
-    output reg rx_err
+    output reg [7:0] data,
+    output reg valid,
+    output reg err
 );
     reg [1:0] state;
     reg ps2_clk_negedge;
@@ -26,42 +26,42 @@ module ps2_controller (
                     state <= 1;
                 end
                 else begin 
-                    rx_valid <= 0;
-                    rx_err <= 0;
-                    rx_data <= 0;
+                    valid <= 0;
+                    err <= 0;
+                    data <= 0;
                     state <= 0;
                 end
             end
             1: begin 
                 if (bit_pos == 7) begin 
-                    rx_data[bit_pos] <= ps2_data;
+                    data[bit_pos] <= ps2_data;
                     parity_chk <= parity_chk ^ ps2_data;
                     state <= 2;
                 end
                 else begin 
-                    rx_data[bit_pos] <= ps2_data;
-                    bit_pos <= bit_pos = 1;
+                    data[bit_pos] <= ps2_data;
+                    bit_pos <= bit_pos + 1;
                     parity_chk <= parity_chk ^ ps2_data;
                     state <= 1;
                 end
             end
             2: begin 
                 if (parity_chk == ps2_data) begin 
-                    rx_valid <= 1;
-                    rx_err <= 0;
+                    valid <= 1;
+                    err <= 0;
                     state <= 0;
                 end
                 else begin
-                    rx_err <= 1;
-                    rx_valid <= 1;
+                    err <= 1;
+                    valid <= 1;
                     state <= 0;
                 end
             end
             default: begin 
                 state <= 0;
-                rx_valid <= 0;
-                rx_err <= 0;
-                rx_data <= 0;
+                valid <= 0;
+                err <= 0;
+                data <= 0;
             end
         endcase
     end
